bb81045d95465ecea52a68605123603cb95c6171
lijingfu <lijingfu@unnoo.com>
2021-12-18 22:46:07 +0800

    lame MP3 编码库学习

---
+++ LameMp3Codec/LameMp3Codec/LameMp3Codec.h
@@ -0,0 +1,19 @@
+//
+//  LameMp3Codec.h
+//  LameMp3Codec
+//
+//  Created by LJF on 2021/12/18.
+//  Copyright © 2021 LJF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LameMp3Codec.
+FOUNDATION_EXPORT double LameMp3CodecVersionNumber;
+
+//! Project version string for LameMp3Codec.
+FOUNDATION_EXPORT const unsigned char LameMp3CodecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LameMp3Codec/PublicHeader.h>
+
+
